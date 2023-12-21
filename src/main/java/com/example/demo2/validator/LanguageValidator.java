80fe0fc5de1bd4769064136d2f812fdfd272b4b0
phuochao091101 <ht0346nt@gmail.com>
2023-12-21 21:31:57 +0700

    21/12/2023

--- src/main/java/com/example/demo2/validator/LanguageValidator.java
+++ src/main/java/com/example/demo2/validator/LanguageValidator.java
@@ -4,10 +4,30 @@
  */
 package com.example.demo2.validator;
 
+import javax.faces.application.FacesMessage;
+import javax.faces.component.UIComponent;
+import javax.faces.component.UIInput;
+import javax.faces.component.UISelectMany;
+import javax.faces.context.FacesContext;
+import javax.faces.validator.FacesValidator;
+import javax.faces.validator.Validator;
+import javax.faces.validator.ValidatorException;
+
 /**
  *
  * @author macbook
  */
-public class LanguageValidator {
+@FacesValidator("LanguageValidator")
+public class LanguageValidator implements Validator{
+
+    @Override
+    public void validate(FacesContext fc, UIComponent uic, Object o) throws ValidatorException {
+        String chkPhone= (String)((UIInput) uic.findComponent("txtwPhone")).getValue();
+        if(chkPhone==null){
+            FacesMessage m=new FacesMessage("Require Message");
+            m.setSeverity(FacesMessage.SEVERITY_ERROR);
+            throw new ValidatorException(m);
+        } // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
     
 }
